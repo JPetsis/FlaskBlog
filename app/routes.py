7cfcaeecb55da16f59ee857a6580afe7867fc9f9
JPetsis <snowblind777@gmail.com>
2020-03-16 16:53:30 +0100

    basic template creation

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello World"
+  user = {'username': 'John'}
+  posts = [
+    {
+      'author': {'username': 'Adam'},
+      'body': 'Beautiful day in Berlin!'
+    },
+    {
+      'author': {'username': 'Susan'},
+      'body': 'The Avengers movie was so cool!'
+    }
+  ]
+  return render_template('index.html', title='home', user=user, posts=posts)
