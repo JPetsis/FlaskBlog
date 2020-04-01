006994390dcd81cb46c0d58607312b9d47eec311
JPetsis <snowblind777@gmail.com>
2020-04-01 13:12:06 +0200

    Database and Users,Posts table creation

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
