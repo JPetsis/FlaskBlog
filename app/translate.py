d9e12f042a8a026d01b7875e959cb4784a95b99f
JPetsis <snowblind777@gmail.com>
2020-05-18 12:05:47 +0200

    Better application structure

--- app/translate.py
+++ app/translate.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from flask_babel import _
-from app import app
+from flask import current_app
 from yandex_translate import YandexTranslate
 
 
@@ -10,11 +10,11 @@ def translate(text, source_language, dest_language):
     # Yandex doesn't require wrapping key in auth
     # Yandex returns a dict of lists, the translation is found in ['text'][0]
     if (
-        "YANDEX_TRANSLATOR_KEY" not in app.config
-        or not app.config["YANDEX_TRANSLATOR_KEY"]
+        "YANDEX_TRANSLATOR_KEY" not in current_app.config
+        or not current_app.config["YANDEX_TRANSLATOR_KEY"]
     ):
         return _("Error: the translation service is not configured.")
-    t = YandexTranslate(app.config["YANDEX_TRANSLATOR_KEY"])
+    t = YandexTranslate(current_app.config["YANDEX_TRANSLATOR_KEY"])
     r = t.translate(text, source_language + "-" + dest_language)
     if r["code"] != 200:
         return _("Error: the translation service failed. %r" % (r["code"]))
